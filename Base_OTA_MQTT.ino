cdb2d54c24a9f7ed967cceb919b5cadc59813dce
domi77185 <tel.domi@gmail.com>
2020-09-08 11:11:49 +0200

    Update Base_OTA_MQTT.ino

--- Base_OTA_MQTT.ino
+++ Base_OTA_MQTT.ino
@@ -1,11 +1,10 @@
 
-#include "connect.h"
-// const char* ssid = "_MON_SSID_";                   // SSID du réseau Wifi
-// const char* password = "_MOT_DE_PASSE_WIFI_";      // Mot de passe du réseau Wifi.
-// const char* mqtt_server = "_IP_DU_BROKER_";         // Adresse IP ou DNS du Broker.
-// const int mqtt_port = 1883;                         // Port du Brocker MQTT
-// const char* mqtt_login = "_LOGIN_";                 // Login de connexion à MQTT.
-// const char* mqtt_password = "_PASSWORD_";           // Mot de passe de connexion à MQTT.
+const char* ssid = "_MON_SSID_";                   // SSID du réseau Wifi
+const char* password = "_MOT_DE_PASSE_WIFI_";      // Mot de passe du réseau Wifi.
+const char* mqtt_server = "_IP_DU_BROKER_";         // Adresse IP ou DNS du Broker.
+const int mqtt_port = 1883;                         // Port du Brocker MQTT
+const char* mqtt_login = "_LOGIN_";                 // Login de connexion à MQTT.
+const char* mqtt_password = "_PASSWORD_";           // Mot de passe de connexion à MQTT.
 // ------------------------------------------------------------
 // Variables de configuration :
 const char* topicIn     = "domoticz/out";             // Nom du topic envoyé par Domoticz
